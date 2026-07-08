2756a5bf875c1ea4a94caefe1ffff942b77ba8e7
Tanya Singhal <tanya.singhal@hitouchcx.com>
2026-07-08 16:26:36 +0530

    Add Vercel Analytics

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -5,11 +5,13 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './redux/store.js'
+import { Analytics } from '@vercel/analytics/react'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <Provider store={store}>
     <App />
+    <Analytics />
     </Provider>
     </BrowserRouter>
   </StrictMode>,
